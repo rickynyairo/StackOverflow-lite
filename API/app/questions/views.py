5bdfd9004683e361ab609d99243574e79e8b84cf
Ricky Nyairo <rickynyairo@gmail.com>
2018-08-17 05:19:08 +0300

    [Finishes #159831786] completed seperating app into modules

--- API/app/questions/views.py
+++ API/app/questions/views.py
@@ -2,10 +2,29 @@ import json
 from datetime import datetime
 from flask import request, jsonify, abort
 from . import questions
+from app.data import data
 
+def locate(id, items):
+    """This function takes 2 arguments (id : int and items : string)
+        To locate the item, question or user, with the identifier, id, 
+        from the collection of items: either questions or users."""
+    collection = data[items]
+    required_item = {}
+    found = False
+    index = 0
+    for i in range(len(collection)):
+        if int(collection[i]['id']) == int(id):
+            required_item = collection[i]
+            found = True
+            index = i
+    if found:
+        return (required_item, index)
+    else:
+        return None
+        
 @questions.route('/api/v1/questions/', methods=['POST', 'GET'])
 def get_questions():
-    """this route handles request to the questions resource"""
+    """This function handles request to the questions resource"""
     if request.method == 'GET':
         # return all questions in the db
         response = jsonify(data['questions'])
@@ -31,26 +50,44 @@ def get_questions():
         response.status_code = 201
 
     return response
-
-@questions.route('/api/v1/questions/<int:id>', methods=['GET'])
+    
+@questions.route('/api/v1/questions/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def question(id, **kwargs):
-    """This function retrieves a question, given a particular id"""
-    questions = data['questions']
-    question = {}
-    for q in questions:
-        if int(q['id']) == int(id):
-            question = q 
-
-    if len(question) == 0:
+    """This function, given a particular question id,
+        retrieves the question or edits the question"""
+    response = {}
+    # locate the question
+    q = locate(int(id), "questions")
+    
+    if q is None:
         # question not found
         # return error 404
         abort(404)
-    else:
+
+    if request.method == 'GET':
         # return the question
-        response = jsonify(question)
+        response = jsonify(q[0])
+        response.status_code = 200
+    elif request.method == 'PUT':
+        # obtain the required edit
+        edited_question = json.loads(request.data.decode('utf-8').replace("'", '"'))['text']
+        # edit the question in the data store
+        q[0]['text'] = edited_question
+        response = jsonify({
+                    "id":id,
+                    "text":edited_question
+                    })
+        response.status_code = 200
+    else:
+        # delete the question
+        question_index = q[1]
+        del data["questions"][question_index]
+        response = jsonify({
+            "question_id":id,
+            "action":"deleted"})
         response.status_code = 200
 
-        return response
+    return response
 
 @questions.route('/api/v1/questions/<int:id>/answers', methods=['POST'])
 def answer(id, **kwargs):
@@ -61,14 +98,11 @@ def answer(id, **kwargs):
     answer = json.loads(request.data.decode('utf-8').replace("'", '"'))
     # initialize up votes to 0
     answer['up_votes'] = "0"
-    found = False
-    for q in questions:
-        if int(q['id']) == int(id):
-            # append the received answer to the answers list
-            found = True
-            question = q
-            question['answers'].append(answer)
-    if found:
+    # locate the question
+    question = locate(int(id), "questions")[0]
+    if question:
+        # question is located, append answer
+        question['answers'].append(answer)
         # return a response with the question id and the answer
         response = jsonify({
             "question_id":str(question['id']),
@@ -80,88 +114,3 @@ def answer(id, **kwargs):
         # the question with the given id was not found
         # return error 404
         abort(404)
-        
-
-    return response
-
-data = {
-    "users":[
-        {
-            "email":"john_doe@gmail.com",
-            "username":"johndoe",
-            "password":"¥«,Â\u008aÝ"
-        },
-        {
-            "email":"rickynyairo@gmail.com",
-            "username":"ricky",
-            "password":"¥«,Â\u008aÝ"
-        },
-
-        ],
-    "questions":[
-        {
-            "id":"1000",
-            "text":"What is the distance from the earth to the moon?",
-            "askedBy":"Jimmy",
-            "answers":[
-                {
-                    "text":"384000 km",
-                    "up_votes":"10000",
-                    "answered_by":"Nancy"
-                },
-                {
-                    "text":"100000km",
-                    "up_votes":"20",
-                    "answered_by":"Brian"
-                }
-            ],
-            "date":"December 20, 2016"
-        },
-        {
-            "id":"1001",
-            "text":"Who was the first Kenyan president?",
-            "askedBy":"Nancy",
-            "answers":[
-                {
-                    "text":"Jomo Kenyatta",
-                    "up_votes":"100",
-                    "answered_by":"Ricky"
-                },
-                {
-                    "text":"Uhuru Kenyatta",
-                    "up_votes":"20",
-                    "answered_by":"Jimmy"
-                },
-                {
-                    "text":"Mwai Kibaki",
-                    "up_votes":"0",
-                    "answered_by":"John Doe"
-                }
-            ],
-            "date":"August 17, 2017"
-        }, 
-        {
-            "id":"1002",
-            "text":"What are the most popular programming languages?",
-            "askedBy":"Ricky",
-            "answers":[
-                {
-                    "text":"JavaScript",
-                    "up_votes":"100",
-                    "answered_by":"Ecma"
-                },
-                {
-                    "text":"Python",
-                    "up_votes":"7000",
-                    "answered_by":"Guido Van Rossum"
-                },
-                {
-                    "text":"Java",
-                    "up_votes":"0",
-                    "answered_by":"Crazy Guy"
-                }
-            ],
-            "date":"July 10, 2018"
-        }
-    ]
-}
